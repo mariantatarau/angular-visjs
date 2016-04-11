9c5a19694980cd224d92eacd20d5f8eb8c40266d
mariantatarau <marian.tatarau@assist.ro>
2016-04-11 12:41:05 +0300

    Update angular-vis.js
    wrong, too common with $scope

--- angular-vis.js
+++ angular-vis.js
@@ -42,32 +42,32 @@ angular.module('ngVis', [])
 
                     // If we've actually changed the data set, then recreate the graph
                     // We can always update the data by adding more data to the existing data set
-                    if ($scope.timeline != null) {
-                        $scope.timeline.destroy();
+                    if (scope.timeline != null) {
+                        scope.timeline.destroy();
                     }
 
                     // Create the timeline object
-                    $scope.timeline = new vis.Timeline(element[0], scope.data.items, scope.data.groups, scope.options);
+                    scope.timeline = new vis.Timeline(element[0], scope.data.items, scope.data.groups, scope.options);
 
                     // Attach an event handler if defined
                     angular.forEach(scope.events, function (callback, event) {
                         if (timelineEvents.indexOf(String(event)) >= 0) {
-                            $scope.timeline.on(event, callback);
+                            scope.timeline.on(event, callback);
                         }
                     });
 
                     // onLoad callback
                     if (scope.events != null && scope.events.onload != null &&
                         angular.isFunction(scope.events.onload)) {
-                        scope.events.onload($scope.timeline);
+                        scope.events.onload(scope.timeline);
                     }
                 });
 
                 scope.$watchCollection('options', function (options) {
-                    if ($scope.timeline == null) {
+                    if (scope.timeline == null) {
                         return;
                     }
-                    $scope.timeline.setOptions(options);
+                    scope.timeline.setOptions(options);
                 });
             }
         };
